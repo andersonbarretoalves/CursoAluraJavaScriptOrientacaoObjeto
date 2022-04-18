d9121460dd8aaf53cce40cd9a2f08cba4896f5df
Anderson Barreto Alves <anderson.alves@grupoimne.local>
2022-04-18 16:27:49 -0300

    finalizando

--- index.js
+++ index.js
@@ -1,45 +1,12 @@
-class cliente {
-    nome;
-    cpf;    
-}
+import { cliente } from "./Cliente.js";
+import { contaCorrente } from "./ContaCorrente.js";
 
-class contaCorrente {
-    agencia;
-    // #saldo = 0 https://github.com/tc39/proposal-class-fields#private-fields
-    _saldo = 0;
+const cliente1 = new cliente("Ricardo", 11122233309);
+const cliente2 = new cliente("Alice", 88822233309);
+const contaCorrenteRicardo = new contaCorrente(1001, cliente1);
+contaCorrenteRicardo.depositar(500);
+const conta2 = new contaCorrente(102, cliente2);
 
-    sacar(valor) {
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0){
-            return
-        }
-        this._saldo += valor;        
-    }
-}
-
-const cliente1 = new cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
-
-const cliente2 = new cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
-
-const contaCorrenteRicardo = new contaCorrente();
-
-contaCorrenteRicardo.agencia = 1001;
-
-contaCorrenteRicardo.depositar(-100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
-
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
-
-console.log(contaCorrenteRicardo);
+let valor = 200;
+contaCorrenteRicardo.transferir(valor, conta2);
+console.log(contaCorrente.numeroDeContas);
